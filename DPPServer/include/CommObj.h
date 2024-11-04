6a9cae9a50ed6e51f73591b6c08e7d15b658302a
Lucas Ergh <lucasergh@gmail.com>
2024-11-03 23:52:53 -0500

    finished serialization functions and started working on server-side communication logic

--- DPPServer/include/CommObj.h
+++ DPPServer/include/CommObj.h
@@ -14,17 +14,56 @@
 typedef std::array<unsigned long long, 2> Range;
 typedef std::vector<unsigned long long> PList;
 
-unsigned char* createMsg(Range r){
-    std::vector<std::byte> msg;
+// serializes range message into byte array for transmission
+std::vector<std::byte> createMsg(const Range& r){
+    std::vector<std::byte> msg(19);
 
-    msg[0] = static_cast<std::byte>(RANGE);
+    int offset = 0;
+    uint8_t msgType = 1;
+    uint16_t payloadSize = 2;
+    
+    // message type
+    memcpy(msg.data() + offset, &msgType, sizeof(uint8_t));
+    offset += sizeof(uint8_t);
 
-    memcpy(msg[1], r.begin(), r.size() * 8);
+    // message size
+    memcpy(msg.data() + offset, &payloadSize, sizeof(uint16_t));
+    offset += sizeof(uint16_t);
 
+    // range
+    memcpy(msg.data() + offset, &r[0], sizeof(r[0]));
+    offset += sizeof(r[0]);
+    memcpy(msg.data() + offset, &r[1], sizeof(r[1]));
+
+    return msg;
 }
 
-unsigned char* createMsg(PList l) {
+// serializes prime list into byte array for transmission 
+std::vector<std::byte> createMsg(const PList& primes) {
+    const uint8_t msgType = 2;
+    const uint16_t payloadSize = primes.size();
 
+    // calculate message size
+    size_t size = sizeof(msgType) + sizeof(payloadSize) + payloadSize * sizeof(unsigned long long);
+
+    std::vector<std::byte> msg(size);
+    int offset = 0;
+
+    // message type
+    std::memcpy(msg.data() + offset, &msgType, sizeof(uint8_t));
+    offset += sizeof(uint8_t);
+
+    // payload size
+    std::memcpy(msg.data() + offset, &payloadSize, sizeof(uint16_t));
+    offset += sizeof(uint16_t);
+
+    // list of primes
+    for (const auto& cur : primes) {
+        std::memcpy(msg.data() + offset, &cur, sizeof(unsigned long long));
+        offset += sizeof(unsigned long long);
+    }
+
+    return msg;
 }
 
 /*
