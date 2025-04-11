458be849f143307d8f79c98ca7f3c9cb534f5ebc
Lucas Ergh <lucasergh@gmail.com>
2025-04-11 17:46:43 -0400

    Move ClientHandler class to its own file

---
+++ Server/include/ClientHandler.h
@@ -0,0 +1,47 @@
+#ifndef CLIENTHANDLER_H
+#define CLIENTHANDLER_H
+
+#include "MySockets.h"
+#include <vector>
+#include <array>
+#include <atomic>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+namespace PrimeProcessor{
+
+    class SocketManager;
+
+    class ClientHandler{
+    public:
+        SocketManager* manager;
+
+        SOCKET clientSocket;
+
+        uint8_t header[3];
+        int iSendResult;
+        int iResult;
+        std::vector<std::byte> lastSent;
+        std::array<unsigned long long, 2> lastRange;
+        std::atomic_bool currentlyRunning = true;
+
+    public:
+
+        ClientHandler(SOCKET& s, SocketManager* m);
+
+        // cloeses conenction with client
+        void closeConnection();
+
+        // loop that communicates with clients
+        void clientComs();
+        void commsFailed();
+
+        static int nextKey;
+        int key;
+
+        std::atomic<bool> needsClosedByParent = false;
+    };
+}
+
+#endif
