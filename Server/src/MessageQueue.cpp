d1e7c951ed14164d9338a82c25eec13c9aa71cd9
Lucas Ergh <lucasergh@gmail.com>
2025-04-29 16:28:59 -0400

    Add empty definitions to MessageQueue methods

--- Server/src/MessageQueue.cpp
+++ Server/src/MessageQueue.cpp
@@ -5,3 +5,30 @@
 */
 
 #include "MessageQueue.h"
+
+namespace PrimeProcessor {
+    // Dequeues a message returning a encoded byte array 
+    std::vector<std::byte> MessageQueue::dequeueWork(){
+
+    }
+
+    // Add a new item to the workQueue
+    void MessageQueue::enqueueWork(std::array<unsigned long long, 2> range){
+
+    }
+
+    // Called by the SocketManager when a client is unable to finish a piece of work
+    void MessageQueue::messageFailed(std::vector<std::byte> message){
+
+    }
+
+    // Add primes found to a queue to be retreived and stored
+    void MessageQueue::enqueuePrimesFound(std::vector<unsigned long long>){
+
+    }
+
+    // Returnes a vector of all ranges in progess and in the work queue
+    std::deque<std::array<unsigned long long, 2>> MessageQueue::emergencyDequeue(){
+
+    }
+}
