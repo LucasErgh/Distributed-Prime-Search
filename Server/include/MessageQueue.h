6bcac769f38fcc8ace76afd9e9e689813237920f
Lucas Ergh <lucasergh@gmail.com>
2025-04-29 18:59:08 -0400

    Add implimentation of MessageQueue methods

--- Server/include/MessageQueue.h
+++ Server/include/MessageQueue.h
@@ -24,26 +24,30 @@ namespace PrimeProcessor {
         std::deque<std::array<unsigned long long, 2>> workQueue;
 
         // Vector of ranges which are currently being searched
-        std::vector<std::array<unsigned long long, 2>> inProgressQueue;
+        std::vector<std::array<unsigned long long, 2>> rangesInProgress;
 
-        std::deque<unsigned long long> primesFound;
+        // Vector of prime numbers found
+        std::vector<unsigned long long> primesFound;
 
     public:
 
         // Dequeues a message returning a encoded byte array 
-        std::vector<std::byte> dequeueWork();
+        std::array<unsigned long long, 2> dequeueWork();
 
-        // Add a new item to the workQueue
-        void enqueueWork(std::array<unsigned long long, 2> range);
+        // Add new items to the workQueue
+        void enqueueWork(std::vector<std::array<unsigned long long, 2>>& ranges);
 
         // Called by the SocketManager when a client is unable to finish a piece of work
-        void messageFailed(std::vector<std::byte> message);
+        void searchFailed(std::array<unsigned long long, 2>& message);
 
         // Add primes found to a queue to be retreived and stored
-        void enqueuePrimesFound(std::vector<unsigned long long>);
+        void enqueuePrimesFound(std::vector<unsigned long long>& primes);
+
+        // Retrieve all primes found currently in vector
+        std::vector<unsigned long long> retreivePrimesFound();
 
         // Returnes a vector of all ranges in progess and in the work queue
-        std::deque<std::array<unsigned long long, 2>> emergencyDequeue();
+        std::vector<std::array<unsigned long long, 2>> emergencyDequeue();
     };
 }
 
--- Server/src/MessageQueue.cpp
+++ Server/src/MessageQueue.cpp
@@ -5,30 +5,69 @@
 */
 
 #include "MessageQueue.h"
+#include <algorithm>
 
 namespace PrimeProcessor {
-    // Dequeues a message returning a encoded byte array 
-    std::vector<std::byte> MessageQueue::dequeueWork(){
 
+    // Dequeues a message returning a encoded byte array 
+    std::array<unsigned long long, 2> MessageQueue::dequeueWork(){
+        std::lock_guard<std::mutex> lock(queueMutex);
+
+        std::array<unsigned long long, 2> back = workQueue.back();
+        rangesInProgress.push_back(back);
+        workQueue.pop_back();
+        return back;
     }
 
-    // Add a new item to the workQueue
-    void MessageQueue::enqueueWork(std::array<unsigned long long, 2> range){
+    // Add new items to the workQueue
+    void MessageQueue::enqueueWork(std::vector<std::array<unsigned long long, 2>>& ranges){
+        std::lock_guard<std::mutex> lock(queueMutex);
 
+        for (auto& cur : ranges) {
+            workQueue.push_front(std::move(cur));
+        }
     }
 
     // Called by the SocketManager when a client is unable to finish a piece of work
-    void MessageQueue::messageFailed(std::vector<std::byte> message){
+    void MessageQueue::searchFailed(std::array<unsigned long long, 2>& message){
+        std::lock_guard<std::mutex> lock(queueMutex);
 
+        rangesInProgress.erase(std::find(rangesInProgress.begin(), rangesInProgress.end(), message));
+        workQueue.push_back(std::move(message));
     }
 
     // Add primes found to a queue to be retreived and stored
-    void MessageQueue::enqueuePrimesFound(std::vector<unsigned long long>){
+    void MessageQueue::enqueuePrimesFound(std::vector<unsigned long long>& primes){
+        std::lock_guard<std::mutex> lock(queueMutex);
 
+        primesFound.insert(primesFound.end(), primes.begin(), primes.end());
+    }
+
+    // Retrieve all primes found currently in vector
+    std::vector<unsigned long long> MessageQueue::retreivePrimesFound(){
+        std::lock_guard<std::mutex> lock(queueMutex);
+
+        std::vector<unsigned long long> primesToReturn = std::move(primesFound);
+        return primesToReturn;
     }
 
     // Returnes a vector of all ranges in progess and in the work queue
-    std::deque<std::array<unsigned long long, 2>> MessageQueue::emergencyDequeue(){
+    std::vector<std::array<unsigned long long, 2>> MessageQueue::emergencyDequeue(){
+        std::lock_guard<std::mutex> lock(queueMutex);
 
+        std::vector<std::array<unsigned long long, 2>> allWorkRanges;
+
+        allWorkRanges.reserve(rangesInProgress.size() + workQueue.size());
+
+        allWorkRanges.insert(allWorkRanges.end(), 
+                             std::make_move_iterator(workQueue.begin()), 
+                             std::make_move_iterator(workQueue.end()));
+
+        allWorkRanges.insert(allWorkRanges.end(),
+                             std::make_move_iterator(rangesInProgress.begin()),
+                             std::make_move_iterator(rangesInProgress.end()));
+
+        return allWorkRanges;
     }
+
 }
